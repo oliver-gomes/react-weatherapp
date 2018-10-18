3b3a1cd1f5a8cf374b5c55fcee20dcf22d9fd100
Oliver Gomes <ogomesphoto@gmail.com>
2018-10-18 19:34:39 -0400

    fetching data

--- src/container/App.js
+++ src/container/App.js
@@ -3,7 +3,17 @@ import Titles from "../components/Titles";
 import Form from "../components/Form";
 import Weather from "../components/Weather";
 
+const API_KEY = "28b2c13348267d3b3a8cbfe9a1905dbd";
+
 class App extends React.Component {
+  getWeather = async () => {
+    const api_call = await fetch(
+      `https://samples.openweathermap.org/data/2.5/forecast?id=524901&appid=${API_KEY}`
+    );
+    const data = await api_call.json();
+    console.log(data);
+  };
+
   render() {
     return (
       <div>
