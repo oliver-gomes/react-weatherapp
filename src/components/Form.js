b941ad495a4e27f02b9a31e588682748868d447d
Oliver Gomes <ogomesphoto@gmail.com>
2018-10-18 19:34:46 -0400

    form implement

--- src/components/Form.js
+++ src/components/Form.js
@@ -2,7 +2,13 @@ import React from "react";
 
 class Form extends React.Component {
   render() {
-    return <div>Form Component</div>;
+    return (
+      <form>
+        <input type="text" name="city" placeholder="City..." />
+        <input type="text" name="country" placeholder="Country..." />
+        <button>Get Weather</button>
+      </form>
+    );
   }
 }
 
