7b6a15677c3c54474bf9d28e0edaba9a0ff4140b
Oliver Gomes <ogomesphoto@gmail.com>
2018-10-18 19:54:33 -0400

    onsubmit setup

--- src/components/Form.js
+++ src/components/Form.js
@@ -3,7 +3,7 @@ import React from "react";
 class Form extends React.Component {
   render() {
     return (
-      <form>
+      <form onSubmit={this.props.getWeather}>
         <input type="text" name="city" placeholder="City..." />
         <input type="text" name="country" placeholder="Country..." />
         <button>Get Weather</button>
