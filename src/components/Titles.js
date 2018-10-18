998b536facded81cec1a6c16472709adc665fb68
Oliver Gomes <ogomesphoto@gmail.com>
2018-10-18 19:06:19 -0400

    Initial commit

---
+++ src/components/Titles.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+class Titles extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Weather Finder</h1>
+        <p>Find out temperature, conditions and more...</p>
+      </div>
+    );
+  }
+}
+
+export default Titles;
