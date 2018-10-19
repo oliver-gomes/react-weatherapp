da5cc12ab570e65c7e80c92d2e7f64a621695d81
Oliver Gomes <ogomesphoto@gmail.com>
2018-10-19 00:01:15 -0400

    bootstrap import

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./container/App.css";
 import "./index.css";
 import App from "./container/App";
 import * as serviceWorker from "./serviceWorker";
