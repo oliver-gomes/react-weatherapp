6008bd27da08863a57d979fb04a66cd8a8d43124
Oliver Gomes <ogomesphoto@gmail.com>
2018-10-18 20:12:37 -0400

    props to update on browser with value

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -2,7 +2,16 @@ import React from "react";
 
 class Weather extends React.Component {
   render() {
-    return <div>Weather Component</div>;
+    return (
+      <div>
+        <p>
+          Location: {this.props.city}, {this.props.country}
+        </p>
+        <p>Temperature: {this.props.temperature}</p>
+        <p>Humidity: {this.props.humidity}</p>
+        <p>Condition: {this.props.description}</p>
+      </div>
+    );
   }
 }
 
