9415b077d694b71bc468594647e8f72b493a5b49
Oliver Gomes <ogomesphoto@gmail.com>
2018-10-19 00:05:36 -0400

    changing temp API to fahrenheit

--- src/container/App.js
+++ src/container/App.js
@@ -20,10 +20,11 @@ class App extends React.Component {
     const city = e.target.elements.city.value;
     const country = e.target.elements.country.value;
     const api_call = await fetch(
-      `https://api.openweathermap.org/data/2.5/weather?q=${city}, ${country}&appid=${API_KEY}`
+      `https://api.openweathermap.org/data/2.5/weather?q=${city}, ${country}&units=imperial&appid=${API_KEY}`
     );
     const data = await api_call.json();
     if (city && country) {
+      console.log(data);
       this.setState({
         temperature: data.main.temp,
         city: data.name,
