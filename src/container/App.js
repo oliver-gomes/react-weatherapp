e1df763222531e8873732842924bb00de7c14b39
Oliver Gomes <ogomesphoto@gmail.com>
2018-10-18 21:01:51 -0400

    error handling update

--- src/container/App.js
+++ src/container/App.js
@@ -23,15 +23,26 @@ class App extends React.Component {
       `https://api.openweathermap.org/data/2.5/weather?q=${city}, ${country}&appid=${API_KEY}`
     );
     const data = await api_call.json();
-    console.log(data);
-    this.setState({
-      temperature: data.main.temp,
-      city: data.name,
-      country: data.sys.country,
-      humidity: data.main.humidity,
-      description: data.weather[0].description,
-      error: ""
-    });
+    if (city && country) {
+      console.log(data);
+      this.setState({
+        temperature: data.main.temp,
+        city: data.name,
+        country: data.sys.country,
+        humidity: data.main.humidity,
+        description: data.weather[0].description,
+        error: ""
+      });
+    } else {
+      this.setState({
+        temperature: undefined,
+        city: undefined,
+        country: undefined,
+        humidity: undefined,
+        description: undefined,
+        error: "Please enter the values"
+      });
+    }
   };
 
   render() {
