58782eec97bd8735f78900c256568d59ea9a88e7
Oliver Gomes <ogomesphoto@gmail.com>
2018-10-18 20:12:22 -0400

    props and setState update

--- src/container/App.js
+++ src/container/App.js
@@ -6,6 +6,15 @@ import Weather from "../components/Weather";
 const API_KEY = "28b2c13348267d3b3a8cbfe9a1905dbd";
 
 class App extends React.Component {
+  state = {
+    temperature: undefined,
+    city: undefined,
+    country: undefined,
+    humidity: undefined,
+    description: undefined,
+    error: undefined
+  };
+
   getWeather = async e => {
     e.preventDefault();
     const city = e.target.elements.city.value;
@@ -15,6 +24,14 @@ class App extends React.Component {
     );
     const data = await api_call.json();
     console.log(data);
+    this.setState({
+      temperature: data.main.temp,
+      city: data.name,
+      country: data.sys.country,
+      humidity: data.main.humidity,
+      description: data.weather[0].description,
+      error: ""
+    });
   };
 
   render() {
@@ -22,7 +39,14 @@ class App extends React.Component {
       <div>
         <Titles />
         <Form getWeather={this.getWeather} />
-        <Weather />
+        <Weather
+          temperature={this.state.temperature}
+          city={this.state.city}
+          country={this.state.country}
+          humidity={this.state.humidity}
+          description={this.state.description}
+          error={this.state.error}
+        />
       </div>
     );
   }
