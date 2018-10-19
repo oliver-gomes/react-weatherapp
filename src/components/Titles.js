381648f16082a99cfdc1e3fe7d4829eaecd4a786
Oliver Gomes <ogomesphoto@gmail.com>
2018-10-19 00:01:45 -0400

    bootstrap class implement

--- src/components/Titles.js
+++ src/components/Titles.js
@@ -2,8 +2,10 @@ import React from "react";
 
 const Titles = () => (
   <div>
-    <h1>Weather Finder</h1>
-    <p>Find out temperature, conditions and more...</p>
+    <h1 className="title-container__title">Weather Finder</h1>
+    <p className="title-container__subtitle">
+      Find out temperature, conditions and more...
+    </p>
   </div>
 );
 
--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,17 +1,33 @@
 import React from "react";
 
 const Weather = props => (
-  <div>
+  <div className="weather__info">
     {props.city &&
       props.country && (
-        <p>
-          Location: {props.city}, {props.country}
+        <p className="weather__key">
+          Location:
+          <span className="weather__value">
+            {props.city}, {props.country}
+          </span>
         </p>
       )}
-    {props.temperature && <p>Temperature: {props.temperature}</p>}
-    {props.humidity && <p>Humidity: {props.humidity}</p>}
-    {props.description && <p>Condition: {props.description}</p>}
-    {props.error && <p>{props.error}</p>}
+    {props.temperature && (
+      <p className="weather__key">
+        Temperature:{" "}
+        <span className="weather__value"> {props.temperature}</span>
+      </p>
+    )}
+    {props.humidity && (
+      <p className="weather__key">
+        Humidity: <span className="weather__value"> {props.humidity}</span>
+      </p>
+    )}
+    {props.description && (
+      <p className="weather__key">
+        Condition: <span className="weather__value"> {props.description}</span>
+      </p>
+    )}
+    {props.error && <p className="weather__error">{props.error}</p>}
   </div>
 );
 
