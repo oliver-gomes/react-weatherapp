983af08ec96b8d95b59550eaf53c66a11b24e7e9
Oliver Gomes <ogomesphoto@gmail.com>
2018-10-18 20:19:51 -0400

    appear only when data selected

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -4,12 +4,15 @@ class Weather extends React.Component {
   render() {
     return (
       <div>
-        <p>
-          Location: {this.props.city}, {this.props.country}
-        </p>
-        <p>Temperature: {this.props.temperature}</p>
-        <p>Humidity: {this.props.humidity}</p>
-        <p>Condition: {this.props.description}</p>
+        {this.props.city &&
+          this.props.country && (
+            <p>
+              Location: {this.props.city}, {this.props.country}
+            </p>
+          )}
+        {this.props.temperature && <p>Temperature: {this.props.temperature}</p>}
+        {this.props.humidity && <p>Humidity: {this.props.humidity}</p>}
+        {this.props.description && <p>Condition: {this.props.description}</p>}
       </div>
     );
   }
